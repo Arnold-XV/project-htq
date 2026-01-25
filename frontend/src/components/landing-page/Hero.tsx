15e28f3f4dfcc6b673166439f45dfba90e1c602f
Diayu <diayunuraini9@gmail.com>
2026-01-26 00:50:11 +0700

    feat(landing-page): change button redirection to google

--- frontend/src/components/landing-page/Hero.tsx
+++ frontend/src/components/landing-page/Hero.tsx
@@ -1,12 +1,11 @@
 'use client';
 import { FaArrowRight } from "react-icons/fa";
 import Image from "next/image";
-import { useRouter } from "next/dist/client/components/navigation";
 import { motion as m } from "framer-motion";
 import { heroActions, heroBadge, heroContainer, heroSubtitle, heroTitle } from "@/lib/motionVariants";
+import { createClient } from "@/lib/supabase/client";
 
 export default function Hero() {
-  const router = useRouter();
 
   return (
     <m.section variants={heroContainer} initial="hidden" animate="visible" viewport={{ once: true }} className="relative w-full h-screen px-6 md:px-16 py-20 md:py-28 text-center overflow-hidden">
@@ -43,7 +42,13 @@ export default function Hero() {
 
         <div className="font-plus-jakarta mt-8 flex flex-col flex-row justify-center gap-6 font-semibold">
           <m.button variants={heroActions} className="bg-[var(--color-primary-button)] text-white px-4 py-2 rounded-lg shadow-lg hover:bg-[var(--color-primary-700)] cursor-pointer inline-flex items-center justify-center"
-          onClick={() => router.push("/auth/login")}>
+          onClick={async () => {await createClient().auth.signInWithOAuth({
+            provider: "google",
+            options: {
+              redirectTo: `${location.origin}/auth/callback`,
+            },
+          });
+          }}>
             Mulai Test <FaArrowRight className="ml-2" />
           </m.button>
           <m.button variants={heroActions} className="border border-[var(--foreground)] px-4 py-2 rounded-lg shadow-lg hover:bg-[var(--color-primary-300)] cursor-pointer text-[var(--foreground)]"
